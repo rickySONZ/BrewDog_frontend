814d699d6d833cca6c1b8781b729b1841fcbc457
Ryan Erricson <ryanerricson@gmail.com>
2021-06-16 16:29:07 -0400

    Added state changes to sign in form

--- src/components/signInForm.js
+++ src/components/signInForm.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { useState } from "react";
 
 const SignInForm = () => {
+
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
     return (
         <div className = "sign-in-div">
             <form className = "sign-in-form">
                 <h1>Login</h1>
-                <input type = "username" placeholder = "Username" /><br></br>
-                <input type = "password" placeholder = "Password" /><br></br>
+                <input type = "username" placeholder = "Username" value={username} onChange={(e) => setUsername(e.target.value)}/><br></br>
+                <input type = "password" placeholder = "Password" value={password} onChange={(e) => setPassword(e.target.value)}/><br></br>
                 <input type = "submit" value="Login" />
             </form>
             
