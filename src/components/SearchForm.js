7c57beeb316565a7a295739367149a736716d321
Ryan Erricson <ryanerricson@gmail.com>
2021-06-18 15:16:11 -0400

    Search breweries is functional

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -3,22 +3,32 @@ import { connect } from 'react-redux';
 
 
 function SearchForm(props) {
+    
+    const initialState = ''
 
-    const [city, setCity] = useState('')
-    const [stateUS, setStateUS] = useState('')
+    const [searchCity, setSearchCity] = useState('')
+    const [searchState, setSearchState] = useState('')
+    const [searchedBreweries, setSearchedBreweries] = useState('');
 
     function handleSubmit(event){
         event.preventDefault()
-        
+        console.log(searchCity)
+        console.log(searchState)
+        let filteredBreweries = props.breweries.filter(b => b.city === searchCity && b.state === searchState)
+        setSearchedBreweries(filteredBreweries)
+        console.log(searchedBreweries)
+        // console.log(searchedBreweries)
+        // setSearchCity("")
+        // setSearchState("")
     }
 
 
     return (
         <div>
             <form className="search-form" onSubmit={handleSubmit}>
-                <input type="text" value={city} placeholder="Search City" onChange={e => setCity(e.target.value)} />
-                <input type="text" value={stateUS} placeholder="Search State" onChange={e => setStateUS(e.target.value)}/>
-                <input type="submit" />
+                <input type="text" value={searchCity} placeholder="Search City" onChange={e => setSearchCity(e.target.value)} />
+                <input type="text" value={searchState} placeholder="Search State" onChange={e => setSearchState(e.target.value)}/>
+                <input type="submit" value="Search For Breweries!" />
             </form>
         </div>
     );
