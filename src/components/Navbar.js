c58f14cd84c2e18a80cb35a21cf4fc959a96644a
Ryan Erricson <ryanerricson@gmail.com>
2021-06-25 10:38:46 -0400

    added logo to nav menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,6 +23,8 @@ function Navbar() {
     return (
         <div className="navbar">
         {/* <img src={process.env.PUBLIC_URL + } />     */}
+            <NavLink className="navlink-logo" to ="/"><img id="nav-link-logo" src={process.env.PUBLIC_URL + '/kisspng-beer-brewing-grains-malts-india-pale-ale-bitter-underdog-5b3160d5c657c4.2574535915299627098124.jpg'}/></NavLink>
+            <NavLink className="navlink-logo-text" to="/"><b>BrewDog</b></NavLink>
             <NavLink className="navlink" to="/">Home</NavLink>
             <NavLink className="navlink" to ={`/users/${currentUser.id}/favorites`}>Favorites</NavLink>
             <NavLink className="navlink" to="/login" onClick={handleClick} >Logout</NavLink>    
